dca3004989a12d0c9f4b209180cece8169b80ce1
=Taufik Aoulad <=aoulad.ben.hmida.el.halka.taufik@alumnat.copernic.cat>
2023-02-21 12:39:15 +0100

    Creacion de estructura para los controllers

--- src/main/java/cat/copernic/CastellersERP/castillo/controllers/a.java
+++ src/main/java/cat/copernic/CastellersERP/castillo/controllers/a.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package cat.copernic.CastellersERP.general;
+package cat.copernic.CastellersERP.castillo.controllers;
 
 /**
  *
  * @author bhugo
  */
-public class d {
+public class a {
     
 }
--- src/main/java/cat/copernic/CastellersERP/gestionEconomica/controllers/e.java
+++ src/main/java/cat/copernic/CastellersERP/gestionEconomica/controllers/e.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package cat.copernic.CastellersERP.clases;
+package cat.copernic.CastellersERP.gestionEconomica.controllers;
 
 /**
  *
  * @author bhugo
  */
-public class b {
+public class e {
     
 }
