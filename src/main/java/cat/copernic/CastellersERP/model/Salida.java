e4a3b045b81e45e34c76b9fecf263c12bdb57df6
capy <garcia.lopez.adrian@alumnat.copernic.cat>
2023-02-21 13:45:10 +0100

    Problema con salida solucionado.

---
+++ src/main/java/cat/copernic/CastellersERP/model/Salida.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cat.copernic.CastellersERP.model;
+
+import lombok.Data;
+
+/**
+ *
+ * @author Taufik
+ */
+
+@Data 
+public class Salida extends Evento{
+    private boolean asistenciaTransporte;
+    private boolean asistenciaSalida;
+    private boolean paradita;
+}
