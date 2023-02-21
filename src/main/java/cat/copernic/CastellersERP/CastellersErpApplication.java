bf6209b15bda871472fe177c774da49587c27677
=Taufik Aoulad <=aoulad.ben.hmida.el.halka.taufik@alumnat.copernic.cat>
2023-02-21 08:26:35 +0100

    First Commit

---
+++ src/main/java/cat/copernic/CastellersERP/CastellersErpApplication.java
@@ -0,0 +1,13 @@
+package cat.copernic.CastellersERP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastellersErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastellersErpApplication.class, args);
+	}
+
+}
