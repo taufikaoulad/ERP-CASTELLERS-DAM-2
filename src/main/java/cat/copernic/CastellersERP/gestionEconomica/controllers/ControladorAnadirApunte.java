5ed7464138ad7b9ec5360177b8a21811254763fd
gomez.beltran.pablo <gomez.beltran.pablo@alumnat.copernic.cat>
2023-02-22 23:25:43 +0100

    navegacion

--- src/main/java/cat/copernic/CastellersERP/gestionEconomica/controllers/ControladorAnadirApunte.java
+++ src/main/java/cat/copernic/CastellersERP/gestionEconomica/controllers/ControladorAnadirApunte.java
@@ -4,6 +4,7 @@
  */
 package cat.copernic.CastellersERP.gestionEconomica.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  *
  * @author pablogomez
  */
+@Controller
 public class ControladorAnadirApunte {
     
     @GetMapping("/AnadirApunte")
