04ae36b7c530692f01c88ac90e9c3b8d9e7654b6
gomez.beltran.pablo <gomez.beltran.pablo@alumnat.copernic.cat>
2023-02-24 08:41:07 +0100

    Corrección Tasca 5A: implementar Thymeleaf al html.
    Tasca 6A: dependències per poder treballar amb Spring Data JPA i MySQL

--- src/main/java/cat/copernic/CastellersERP/gestionEconomica/controllers/ControladorListarApunte.java
+++ src/main/java/cat/copernic/CastellersERP/gestionEconomica/controllers/ControladorListarApunte.java
@@ -4,6 +4,8 @@
  */
 package cat.copernic.CastellersERP.gestionEconomica.controllers;
 
+import cat.copernic.CastellersERP.model.Apunte;
+import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +16,27 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 
 @Controller
-
 public class ControladorListarApunte {
     
     @GetMapping("/gestionEconomica")
     public String inicio(Model model){ 
+        
+        var apunte = new Apunte();
+        apunte.setConcepto("Camisetas");
+        apunte.setPrecio(500);
+        apunte.setTipo("Gasto");
+        
+        var apunte1 = new Apunte();
+        apunte1.setConcepto("Mensualidad");
+        apunte1.setPrecio(1000);
+        apunte1.setTipo("Ingreso");
+        
+        var apuntes = new ArrayList<Apunte>();
+        apuntes.add(apunte);
+        apuntes.add(apunte1);
+        
+        model.addAttribute("apuntes",apuntes);
+        
         
         return "gestionEconomica/ListarApuntes"; 
     }
