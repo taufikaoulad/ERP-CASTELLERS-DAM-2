1f05c64a35cd17d99c137b01b2e6fc33d98eaacb
Hugo Barrera Castillo <barrera.castillo.hugo@alumnat.copernic.cat>
2023-03-14 07:40:39 +0000

    Crud castillo mas habilitar modulos, juntado con develop

---
+++ src/main/java/cat/copernic/CastellersERP/DAO/TipoUsuarioDAO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package cat.copernic.CastellersERP.DAO;
+
+import cat.copernic.CastellersERP.model.TipoUsuario;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author bhugo
+ */
+public interface TipoUsuarioDAO extends CrudRepository<TipoUsuario,Integer>{
+    
+}
