fd93283b7620db49d15d779653897c0ff4e8bebd
gomez.beltran.pablo <gomez.beltran.pablo@alumnat.copernic.cat>
2023-03-20 09:55:58 +0100

    commit para hacer pull de develop

--- src/main/java/cat/copernic/CastellersERP/general/controllers/ControladorListarUsuario.java
+++ src/main/java/cat/copernic/CastellersERP/general/controllers/ControladorListarUsuario.java
@@ -8,14 +8,12 @@ import cat.copernic.CastellersERP.general.serveis.UsuarioService;
 import cat.copernic.CastellersERP.model.Usuario;
 import cat.copernic.CastellersERP.utils.EncriptadorContrasenya;
 import jakarta.validation.Valid;
-import org.aspectj.weaver.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import static org.thymeleaf.spring6.util.FieldUtils.errors;
 
 /**
  *
