2e16f410bf5f2c4d044bd4a763d6d04e86d5bf2e
=Hugo Barrera <=barrera.castillo.hugo@alumnat.copernic.cat>
2023-03-22 08:20:20 +0100

    antes de pull

--- src/main/java/cat/copernic/CastellersERP/ensayo/controllers/ControladorEnsayos.java
+++ src/main/java/cat/copernic/CastellersERP/ensayo/controllers/ControladorEnsayos.java
@@ -69,7 +69,7 @@ public class ControladorEnsayos {
     public String editarEnsayo(Ensayo ensayo, Model model) {
 
         model.addAttribute("ensayo", ensayoService.buscarEnsayo(ensayo));
-
+ 
         return "ensayo/FormularioEnsayo";
     }
 
