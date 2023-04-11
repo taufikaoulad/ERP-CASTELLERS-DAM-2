672913a828726b1e8ff57c24fe0ff93ea0df6b6e
=Hugo Barrera <=barrera.castillo.hugo@alumnat.copernic.cat>
2023-04-11 11:48:41 +0200

    comentado castilloService

--- src/main/java/cat/copernic/CastellersERP/castillo/serveis/CastilloServiceInterface.java
+++ src/main/java/cat/copernic/CastellersERP/castillo/serveis/CastilloServiceInterface.java
@@ -8,19 +8,50 @@ import cat.copernic.CastellersERP.model.Castillo;
 import java.util.List;
 
 /**
- *
  * @author bhugo
  */
 public interface CastilloServiceInterface {
-    
+
+    /**
+     *
+     * Devuelve una lista de todos los castillos almacenados en la base de
+     * datos.
+     *
+     * @return una lista de objetos Castillo
+     */
     public List<Castillo> listarCastillos();
-    
+
+    /**
+     *
+     * Agrega un nuevo castillo a la base de datos.
+     *
+     * @param castillo el objeto Castillo a agregar
+     */
     public void agregarCastillo(Castillo castillo);
-    
+
+    /**
+     *
+     * Elimina un castillo de la base de datos.
+     *
+     * @param castillo el objeto Castillo a eliminar
+     */
     public void eliminarCastillo(Castillo castillo);
-    
+
+    /**
+     *
+     * Busca un castillo en la base de datos en función de sus propiedades.
+     *
+     * @param castillo el objeto Castillo con las propiedades a buscar
+     * @return el objeto Castillo que cumple con los criterios de búsqueda
+     */
     public Castillo buscarCastillo(Castillo castillo);
-    
+
+    /**
+     *
+     * Busca un castillo en la base de datos en función de su ID.
+     *
+     * @param idCastillo el ID del castillo a buscar
+     * @return el objeto Castillo con el ID proporcionado
+     */
     public Castillo buscarCastilloPorId(int idCastillo);
-    
 }
