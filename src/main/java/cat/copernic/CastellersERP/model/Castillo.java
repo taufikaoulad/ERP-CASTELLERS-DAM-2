a3325f5fdcfc5df9293d6b223e936a990ab3daac
Hugo Barrera Castillo <barrera.castillo.hugo@alumnat.copernic.cat>
2023-04-13 09:46:25 +0000

    Comentarios codigo hugo y traduccion al catalan de html

--- src/main/java/cat/copernic/CastellersERP/model/Castillo.java
+++ src/main/java/cat/copernic/CastellersERP/model/Castillo.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package cat.copernic.CastellersERP.model;
 
 import jakarta.persistence.*;
@@ -12,45 +8,66 @@ import java.util.ArrayList;
 import java.util.List;
 import lombok.Data;
 
-/**
- *
- * @author bhugo
- */
-
 @Data
 @Entity
 @Table(name = "castillo")
-public class Castillo implements Serializable{
+public class Castillo implements Serializable {
+
     @Id //Indica al sistema que l'atribut idgos és la clau primària de la BBDD
-    @GeneratedValue(strategy=GenerationType.IDENTITY) //Indica al sistema com generem l'id, en el nostre cas autoincremental, per això fem servir IDENTITY
-    @Column(name="idcastillo")
-    private int idCastillo; 
-    
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Indica al sistema com generem l'id, en el nostre cas autoincremental, per això fem servir IDENTITY
+    @Column(name = "idcastillo")
+    private int idCastillo;
+
+    /**
+     * El nombre del castillo.
+     */
     @NotEmpty
-    @Column(name="nombre")
+    @Column(name = "nombre")
     private String nombre;
-    
+
+    /**
+     * El número de pisos del castillo.
+     */
     @Min(value = 0)
-    @Column(name="pisos")
+    @Column(name = "pisos")
     private int pisos;
-    
+
+    /**
+     * El número de personas por piso del castillo.
+     */
     @Min(value = 0)
-    @Column(name="personaspisos")
+    @Column(name = "personaspisos")
     private int personasPisos;
-    
+
+    /**
+     * El número de piñas del castillo.
+     */
     @Min(value = 0)
-    @Column(name="ncpina")
+    @Column(name = "ncpina")
     private int ncPina;
-    
+
+    /**
+     * El número de troncos del castillo.
+     */
     @Min(value = 0)
-    @Column(name="nctronc")
+    @Column(name = "nctronc")
     private int ncTronc;
-    
-    @Column(name="aixecat")
+
+    /**
+     * Indica si el castillo está levantado o no.
+     */
+    @Column(name = "aixecat")
     private boolean aixecat;
-    @Column(name="agulla")
+
+    /**
+     * Indica si el castillo tiene aguja o no.
+     */
+    @Column(name = "agulla")
     private boolean agulla;
-    
+
+    /**
+     * Lista de eventos a los que está asignado el castillo.
+     */
     @ManyToMany(cascade = {CascadeType.ALL}, mappedBy = "castillosAsignados")
     private List<Evento> eventos = new ArrayList<>();
 }
