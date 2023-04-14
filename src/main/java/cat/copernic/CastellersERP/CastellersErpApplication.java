cce986535ebc2d4f0861150ff6d5fcf8b0860e92
=Taufik Aoulad <=aoulad.ben.hmida.el.halka.taufik@alumnat.copernic.cat>
2023-04-14 09:14:49 +0200

    TASCA 10B: Se ha generado el JavaDoc Correctamente.

--- src/main/java/cat/copernic/CastellersERP/CastellersErpApplication.java
+++ src/main/java/cat/copernic/CastellersERP/CastellersErpApplication.java
@@ -3,13 +3,12 @@ package cat.copernic.CastellersERP;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//main
 @SpringBootApplication
 public class CastellersErpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CastellersErpApplication.class, args);
-                
-                
                 
 	}
 
